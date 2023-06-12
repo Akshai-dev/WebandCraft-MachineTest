5f73f8402fefbc57d2c518d5810163fd2a93c9e3
Akshai <akshai.kunjumon@gmail.com>
2023-06-12 14:09:04 +0530

    Initial Commit

---
+++ WebandCraft MachineTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebandCraft MachineTest
+//
+//  Created by apple on 12/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
